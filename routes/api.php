59e08d630be9a430a48ca801ce66e6bf69a38931
cygon23 <rweyemamu088@gmail.com>
2024-11-02 20:12:03 +0300

    api auth user register,login,logout

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\AuthController;
 use App\Http\Controllers\api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,7 +8,17 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::apiResource('products',ProductController::class);
+
+
+Route::post('/login',[AuthController::class,'login'])->middleware('throttle:5,1');
+Route::post('/register',[AuthController::class,'register']);
+
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::post('/logout',[AuthController::class,'logout']);
+    Route::get('/profile',[AuthController::class,'profile']);
+    Route::apiResource('products',ProductController::class);
+
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
